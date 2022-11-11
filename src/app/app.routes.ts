425988f1039e4ac7d00b935d8d3a4dfb9475e640
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-11-10 22:24:21 -0400

    cadastro.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
+import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+
 
 export const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent},
-    { path: 'sobre', component: SobreComponent }
+    { path: 'sobre', component: SobreComponent },
+    { path: 'cadastro', component: CadastroComponent }
 ];
