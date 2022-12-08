21880351b31723b549301b06b9acab0ab4d87242
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-12-07 23:24:58 -0400

    component

--- src/app/demos/arquitetura-componentes/produto.route.ts
+++ src/app/demos/arquitetura-componentes/produto.route.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProdutoDashboardComponent } from './produto-dashboard/produto-dashboard.component';
+import { EditarProdutoComponent } from './editar-produto/editar-produto.component';
+import { ProdutoAppComponent } from './produto.app.component';
+import { ProdutosResolve } from './services/produto.resolve';
 
 const produtoRouterConfig: Routes = [
-    {path: '', component: ProdutoDashboardComponent}
+    {
+        path: '', component: ProdutoAppComponent,
+        children: [
+            { path: '', redirectTo: 'todos' },
+            { 
+                path: ':estado', 
+                component: ProdutoDashboardComponent,
+                resolve: {
+                    produtos: ProdutosResolve
+                },
+                data: {
+                    teste: 'informação'
+                }
+            },
+            { path: 'editar/:id', component: EditarProdutoComponent }
+        ]
+    }
 ];
 
 @NgModule({
-    imports:[
+    imports: [
         RouterModule.forChild(produtoRouterConfig)
     ],
-    exports:[RouterModule]
-
+    exports: [RouterModule]
 })
-export class ProdutoRoutingModule{}
+export class ProdutoRoutingModule { }
